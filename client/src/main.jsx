b5aa2631979929edafb40ed777af089ac5e01dbd
Piyush <piyush15raj203@gmail.com>
2026-05-28 14:08:48 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
+import './index.css'
+import App from './App.jsx'
+import Steps from './components/Steps.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <AppContextProvider>
+      <App />
+  </AppContextProvider>
+
+  </BrowserRouter>
+    
+  
+)
