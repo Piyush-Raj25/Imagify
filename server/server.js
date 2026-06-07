8382977ee5a3178d2c66e0c83beafe997ad902e1
Piyush <piyush15raj203@gmail.com>
2026-06-07 14:23:22 +0530

    fix: cors origin

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app=express()
 
 app.use(express.json())
 app.use(cors({
-  origin: "https://imagify-o9i2vmilm-piyush-rajs-projects-ac95fc8e.vercel.app"
+  origin: process.env.FRONTEND_URL,
 }))
 await connectDB()
 
